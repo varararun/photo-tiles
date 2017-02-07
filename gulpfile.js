6fcf8b02b0066eb1d302b5b7bbb49d835b9689ee
Arun Varghese <avarghese4790@users.noreply.github.com>
2017-02-07 12:21:44 -0600

    added support for hound, eslint, scss-lint, packaged new js/css (#1)

--- gulpfile.js
+++ gulpfile.js
@@ -17,124 +17,145 @@ var del = require("del");
 var runSequence = require("run-sequence");
 
 var banner = ["/*\n",
-    " * <%= pkg.title %> (<%= pkg.homepage %>)\n",
-    " * Copyright 2016-" + (new Date()).getFullYear(), " <%= pkg.author %>\n",
-    " */\n",
-    ""
+  " * <%= pkg.title %> (<%= pkg.homepage %>)\n",
+  " * Copyright 2016-" + (new Date()).getFullYear(), " <%= pkg.author %>\n",
+  " */\n",
+  ""
 ].join("");
 
-gulp.task("clean", function(){
-    return del(["dist"]);
+gulp.task("clean", function () {
+  return del.sync(["dist"], {
+    force: true
+  });
 });
 
 gulp.task("default", ["serve"]);
 
-gulp.task("minify", function(){
-    runSequence("scss", "format", "minify-css", "typescript", "minify-js");
+gulp.task("minify", function () {
+  runSequence("scss", "minify-css", "sourcemap-css", "typescript", "minify-js", "sourcemap-js");
 });
 
 gulp.task("typescript", function () {
-    tsProject.src()
-        .pipe(tsProject())
-        .js.pipe(gulp.dest("assets/js"));
+  tsProject.src()
+    .pipe(tsProject())
+    .js.pipe(gulp.dest("assets/js"));
 });
 
 gulp.task("scss", function () {
-    gulp.src("assets/css/**/*.scss")
-        .pipe(sass().on("error", sass.logError))
-        .pipe(autoprefixer({
-            browsers: ["last 2 versions", "> 5%", "Firefox ESR"]
-        }))
-        .pipe(gulp.dest("assets/css"));
+  gulp.src("assets/css/*.scss")
+    .pipe(sass().on("error", sass.logError))
+    .pipe(autoprefixer({
+      browsers: ["last 2 versions", "> 5%", "Firefox ESR"]
+    }))
+    .pipe(gulp.dest("assets/css"));
 });
 
 gulp.task("jshint", function () {
-    gulp.src([
-            "assets/js/photo-tiles.js",
-            "*.js"
-        ])
-        .pipe(jshint())
-        .pipe(jshint.reporter(stylish))
-        .pipe(jshint.reporter("fail"));
+  gulp.src([
+      "assets/js/photo-tiles.js"
+    ])
+    .pipe(jshint())
+    .pipe(jshint.reporter(stylish))
+    .pipe(jshint.reporter("fail"));
 });
 
 gulp.task("format", function () {
-    gulp.src([
-            "assets/css/photo-tiles.css",
-            "client/js/photo-tiles.js",
-            "index.html",
-            "*.{js,json}"
-        ], {
-            base: "./"
-        })
-        .pipe(beautify())
-        .pipe(gulp.dest("./"));
+  gulp.src([
+      "assets/css/photo-tiles.css",
+      "client/js/photo-tiles.js",
+      "index.html",
+      "*.{js,json}"
+    ], {
+      base: "./"
+    })
+    .pipe(beautify())
+    .pipe(gulp.dest("./"));
 });
 
-gulp.task("minify-css", ["scss"], function () {
-    gulp.src([
-            "assets/css/photo-tiles.css"
-        ])
-        .pipe(sourcemaps.init())
-        .pipe(cleanCSS({
-            compatibility: "ie8"
-        }))
-        .pipe(rename({
-            suffix: ".min"
-        }))
-        .pipe(sourcemaps.write())
-        .pipe(header(banner, {
-            pkg: pkg
-        }))
-        .pipe(gulp.dest("assets/css"));
+gulp.task("sourcemap-css", function () {
+  gulp.src([
+      "assets/css/*.min.css"
+    ])
+    .pipe(sourcemaps.init())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest("assets/css"));
+});
+
+gulp.task("sourcemap-js", function () {
+  gulp.src([
+      "assets/js/*.min.js",
+      "!assets/js/*spec.js"
+    ])
+    .pipe(sourcemaps.init())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest("assets/js"));
+});
+
+gulp.task("minify-css", function () {
+  gulp.src([
+      "assets/css/photo-tiles.css"
+    ])
+    .pipe(sourcemaps.init())
+    .pipe(cleanCSS({
+      compatibility: "ie8"
+    }))
+    .pipe(rename({
+      suffix: ".min"
+    }))
+
+    .pipe(header(banner, {
+      pkg: pkg
+    }))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest("assets/css"));
 });
 
 gulp.task("minify-js", function () {
-    gulp.src([
-            "assets/js/photo-tiles.js"
-        ])
-        .pipe(sourcemaps.init())
-        .pipe(uglify())
-        .pipe(rename({
-            suffix: ".min"
-        }))
-        .pipe(sourcemaps.write())
-        .pipe(header(banner, {
-            pkg: pkg
-        }))
-        .pipe(gulp.dest("assets/js"));
+  gulp.src([
+      "assets/js/photo-tiles.js"
+    ])
+    .pipe(sourcemaps.init())
+    .pipe(uglify())
+    .pipe(rename({
+      suffix: ".min"
+    }))
+    .pipe(header(banner, {
+      pkg: pkg
+    }))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest("assets/js"));
 });
 
 gulp.task("browserSync", function () {
-    browserSync.init({
-        server: {
-            baseDir: ""
-        },
-    });
+  browserSync.init({
+    server: {
+      baseDir: ""
+    },
+  });
 });
 
 gulp.task("package", function () {
-    runSequence("clean", "scss", "minify-css", "typescript", "minify-js");
-    gulp.src([
-            "assets/js/*.{js,map}",
-            "!assets/js/*.spec.js"
-        ])
-        .pipe(gulp.dest("dist/js"));
-    gulp.src([
-            "assets/css/*.{css,map}"
-        ])
-        .pipe(gulp.dest("dist/css"));
+  runSequence("clean", "minify");
+  gulp.src([
+      "assets/js/*.{js,map}",
+      "!assets/js/*.spec.js"
+    ])
+    .pipe(gulp.dest("dist/js"));
+  gulp.src([
+      "assets/css/*.{css,map}"
+    ])
+    .pipe(gulp.dest("dist/css"));
 });
 
 gulp.task("serve", function () {
-    runSequence("browserSync", "scss", "minify-css", "typescript", "minify-js");
+  runSequence("browserSync", "minify");
 
-    gulp.watch("assets/css/**/*.scss", ["scss"]);
-    gulp.watch("assets/css/photo-tiles.css", ["minify-css"]);
-    gulp.watch("assets/js/photo-tiles.ts", ["typescript"]);
-    gulp.watch("assets/js/photo-tiles.js", ["minify-js"]);
-    gulp.watch("index.html", browserSync.reload);
-    gulp.watch("assets/**/*.{js,css}", browserSync.reload);
+  gulp.watch("assets/css/**/*.scss", ["scss"]);
+  gulp.watch("assets/css/photo-tiles.css", ["minify-css"]);
+  gulp.watch("assets/js/photo-tiles.ts", ["typescript"]);
+  gulp.watch("assets/js/photo-tiles.js", ["minify-js"]);
+  gulp.watch("index.html", browserSync.reload);
+  gulp.watch("assets/**/*.{js,css}", browserSync.reload);
 });
 
 module.exports = gulp;
